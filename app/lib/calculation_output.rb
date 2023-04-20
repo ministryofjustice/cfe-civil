ad55277e9eed62b7da2e9a2101d289afc670f7dc
Stephen Dicks <github@starswan.com>
2023-04-20 10:01:25 +0100

    LEP-99 add combined disputed and combined non disputed capital fields (#216)

--- app/lib/calculation_output.rb
+++ app/lib/calculation_output.rb
@@ -1,10 +1,11 @@
 class CalculationOutput
   delegate :dependant_allowance, :partner_dependant_allowance, to: :@disposable_income_subtotals
 
-  def initialize(gross_income_subtotals: nil, capital_subtotals: nil, disposable_income_subtotals: nil)
-    @capital_subtotals = capital_subtotals || CapitalSubtotals.new
-    @gross_income_subtotals = gross_income_subtotals || GrossIncomeSubtotals.new
-    @disposable_income_subtotals = disposable_income_subtotals || DisposableIncomeSubtotals.new
+  def initialize(gross_income_subtotals: GrossIncomeSubtotals.new,
+                 capital_subtotals: CapitalSubtotals.new, disposable_income_subtotals: DisposableIncomeSubtotals.new)
+    @capital_subtotals = capital_subtotals
+    @gross_income_subtotals = gross_income_subtotals
+    @disposable_income_subtotals = disposable_income_subtotals
   end
 
   attr_reader :capital_subtotals, :gross_income_subtotals
