1df7d53f66fde2446edb9cc163db568f42c38c5b
Stephen Dicks <github@starswan.com>
2023-04-27 08:18:46 +0100

    remove persistence from test

--- spec/services/collators/dependants_allowance_collator_spec.rb
+++ spec/services/collators/dependants_allowance_collator_spec.rb
@@ -2,12 +2,14 @@ require "rails_helper"
 
 module Collators
   RSpec.describe DependantsAllowanceCollator do
-    let(:assessment) { create :assessment, :with_disposable_income_summary }
-    let(:disposable_income_summary) { assessment.disposable_income_summary }
+    let(:submission_date) { Date.current }
 
     subject(:collator) do
-      described_class.call(dependants: dependants.map { |x| DependantWrapper.new(dependant: x, submission_date: assessment.submission_date) },
-                           submission_date: assessment.submission_date)
+      described_class.call(dependants: dependants.map do |x|
+                                         DependantWrapper.new(dependant: x,
+                                                              submission_date:)
+                                       end,
+                           submission_date:)
     end
 
     describe ".call" do
@@ -20,9 +22,9 @@ module Collators
       end
 
       context "with dependants" do
-        let(:dependant1) { create :dependant, :under15, in_full_time_education: true, assessment: }
-        let(:dependant2) { create :dependant, :over18, in_full_time_education: true, assessment: }
-        let(:dependant3) { create :dependant, :aged16or17, in_full_time_education: true, assessment: }
+        let(:dependant1) { build :dependant, :under15, in_full_time_education: true }
+        let(:dependant2) { build :dependant, :over18, in_full_time_education: true }
+        let(:dependant3) { build :dependant, :aged16or17, in_full_time_education: true }
         let(:dependants) { [dependant1, dependant2, dependant3] }
 
         it "returns the under_16s / over_16s grouped together" do
