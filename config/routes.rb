62b65eb9d888c421ee9e1e6757dc5c960333431f
Ripan <er.ripankumar@gmail.com>
2023-05-24 15:15:44 +0100

    LEP-165 delete v5 routes and full assessment spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,6 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => "/api-docs"
   mount Rswag::Api::Engine => "/api-docs"
-  resources :assessments, only: %i[create show] do
-    resource :applicant, only: [:create]
-    resources :capitals, only: [:create]
-    resources :dependants, only: [:create]
-    resources :outgoings, only: [:create]
-    resources :properties, only: [:create]
-    resources :vehicles, only: :create
-    resources :other_incomes, only: [:create]
-    resource :irregular_incomes, only: [:create]
-    resources :state_benefits, only: [:create]
-    resources :explicit_remarks, only: [:create]
-    resources :cash_transactions, only: [:create]
-    resources :employments, only: [:create]
-    resources :proceeding_types, only: [:create]
-    resources :regular_transactions, only: [:create]
-    resources :partner_financials, only: [:create]
-  end
   resources :state_benefit_type, only: [:index]
 
   root to: "main#index"
