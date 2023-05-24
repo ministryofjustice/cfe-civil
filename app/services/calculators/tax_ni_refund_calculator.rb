b37d109a6a3178c0c2292ab2ca9d0475fe84def0
Stephen Dicks <github@starswan.com>
2023-05-24 11:19:49 +0100

    LEP-121 don't create remarks in calculators

--- app/services/calculators/tax_ni_refund_calculator.rb
+++ app/services/calculators/tax_ni_refund_calculator.rb
@@ -1,31 +1,27 @@
 module Calculators
   class TaxNiRefundCalculator
-    def self.call(employment)
-      new(employment).call
-    end
+    KEY_MAPPING = {
+      tax: :employment_tax,
+      national_insurance: :employment_nic,
+    }.freeze
 
-    def initialize(employment)
-      @employment = employment
-    end
+    class << self
+      def call(employment_payments:)
+        employment_payments.flat_map do |payment|
+          attrs = {}
+          attrs[:tax] = 0 if payment.tax >= 0
+          attrs[:national_insurance] = 0 if payment.national_insurance >= 0
 
-    def call
-      @employment.employment_payments.each do |payment|
-        attrs = {}
-        attrs[:tax] = 0 if payment.tax >= 0
-        attrs[:national_insurance] = 0 if payment.national_insurance >= 0
-
-        update_and_add_remarks(attrs, payment) unless attrs.empty?
+          if attrs.empty?
+            []
+          else
+            payment.update!(attrs)
+            attrs.keys.map do |key|
+              RemarksData.new(type: KEY_MAPPING.fetch(key), issue: :refunds, ids: [payment.client_id])
+            end
+          end
+        end
       end
-    end
-
-  private
-
-    def update_and_add_remarks(attrs, payment)
-      payment.update!(attrs)
-      my_remarks = @employment.assessment.remarks
-      my_remarks.add(:employment_tax, :refunds, [payment.client_id]) if attrs.key?(:tax)
-      my_remarks.add(:employment_nic, :refunds, [payment.client_id]) if attrs.key?(:national_insurance)
-      @employment.assessment.update!(remarks: my_remarks)
     end
   end
 end
