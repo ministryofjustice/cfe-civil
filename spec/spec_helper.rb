d1e7e32254a4bc72083a797bf06215c46fc0acb2
Stephen Dicks <github@starswan.com>
2023-05-30 10:08:42 +0100

    reduce coverage percentage after removing code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,7 +27,7 @@ unless ENV["NOCOVERAGE"]
 
     enable_coverage :branch
     primary_coverage :branch
-    minimum_coverage branch: 98.20, line: 99.92
+    minimum_coverage branch: 98.18, line: 99.92
   end
 end
 
