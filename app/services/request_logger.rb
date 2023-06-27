042503c3824c5ffcee1b0a6c6ca1f4c48b42b40c
David Read <david.read@digital.justice.gov.uk>
2023-06-27 15:39:02 +0100

    [LEP-223] Fix for exception on missing partner.partner in input

--- app/services/request_logger.rb
+++ app/services/request_logger.rb
@@ -12,7 +12,9 @@ class RequestLogger
       partner_params = event_params[:partner]
       if partner_params
         partner = partner_params[:partner]
-        partner[:date_of_birth] = redact_dob(now, partner[:date_of_birth])
+        if partner
+          partner[:date_of_birth] = redact_dob(now, partner[:date_of_birth])
+        end
         partner_params.fetch(:dependants, []).each do |d|
           d[:date_of_birth] = redact_dob(now, d[:date_of_birth])
         end
