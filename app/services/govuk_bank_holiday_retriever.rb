f09984b423d99ec7d6965e5126ee0427fc6d580a
Ripan <er.ripankumar@gmail.com>
2023-06-29 14:33:02 +0100

    tidy up

--- app/services/govuk_bank_holiday_retriever.rb
+++ app/services/govuk_bank_holiday_retriever.rb
@@ -10,15 +10,10 @@ class GovukBankHolidayRetriever
 private
 
   def response
-    store = Rails.cache
-    # store = ActiveSupport::Cache.lookup_store(:file_store, "/tmp/cache", expires_in: 5.seconds)
     client = Faraday.new do |builder|
-      builder.use Faraday::HttpCache, store:, strategy: Faraday::HttpCache::Strategies::ByUrl, logger: Rails.logger
+      builder.use Faraday::HttpCache, store: Rails.cache, strategy: Faraday::HttpCache::Strategies::ByUrl, logger: Rails.logger
       builder.adapter Faraday.default_adapter
       builder.response :raise_error
-      # builder.response :json, parser_options: { symbolize_names: false }
-
-      # builder.response :logger, Rails.logger, headers: true, bodies: false, log_level: :debug
     end
 
     @response ||= client.get(uri)
