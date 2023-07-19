f03c237f1f41ba69712ecaeb9eb6a40657be40f1
Ripan <er.ripankumar@gmail.com>
2023-07-19 19:31:43 +0100

    Update multiple_employments_calculator_spec.rb

--- spec/services/calculators/multiple_employments_calculator_spec.rb
+++ spec/services/calculators/multiple_employments_calculator_spec.rb
@@ -6,6 +6,8 @@ module Calculators
     let(:result) { described_class.call(assessment.submission_date) }
 
     it "sets all items to zero apart from allowance" do
+      expect(result.employment).to be_entitles_employment_allowance
+      expect(result.employment).not_to be_entitles_childcare_allowance
       expect(result.employment)
         .to have_attributes(
           monthly_gross_income: 0.0,
