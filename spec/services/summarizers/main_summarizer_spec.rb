2544990287060a03001058f24f026a8b1b579767
Ripan <er.ripankumar@gmail.com>
2023-08-21 13:58:47 +0100

    Update main_summarizer_spec.rb

--- spec/services/summarizers/main_summarizer_spec.rb
+++ spec/services/summarizers/main_summarizer_spec.rb
@@ -10,13 +10,13 @@ module Summarizers
              proceedings: [%w[DA003 A], %w[SE014 Z]]
     end
 
-    subject(:assessor) { described_class.call(assessment:, receives_qualifying_benefit: false, receives_asylum_support: false) }
+    subject(:summarizer) { described_class.call(assessment:, receives_qualifying_benefit: false, receives_asylum_support: false) }
 
     context "AssessmentProceedingTypeAssessor" do
       it "calls AssessmentProceedingTypeAssessor for each proceeding type code" do
         expect(Assessors::AssessmentProceedingTypeAssessor).to receive(:call).with(assessment:, proceeding_type_code: "DA003", receives_qualifying_benefit: false, receives_asylum_support: false)
         expect(Assessors::AssessmentProceedingTypeAssessor).to receive(:call).with(assessment:, proceeding_type_code: "SE014", receives_qualifying_benefit: false, receives_asylum_support: false)
-        assessor
+        summarizer
       end
     end
 
@@ -32,7 +32,7 @@ module Summarizers
       it "calls the Results summarizer to update the assessment result" do
         expect(Utilities::ResultSummarizer).to receive(:call).with(%w[eligible ineligible]).and_call_original
 
-        assessor
+        summarizer
         expect(assessment.assessment_result).to eq "partially_eligible"
       end
     end
