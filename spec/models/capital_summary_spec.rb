c0454b960de5ad0e1310ccd394fb8c1483dff508
Ripan <er.ripankumar@gmail.com>
2023-08-30 15:46:59 +0100

    remove incorrect associations

--- spec/models/capital_summary_spec.rb
+++ spec/models/capital_summary_spec.rb
@@ -1,5 +1,6 @@
 require "rails_helper"
 
 RSpec.describe CapitalSummary do
-  it { is_expected.to have_many(:eligibilities) }
+  it { is_expected.to belong_to(:assessment) }
+  it { is_expected.to have_many(:eligibilities).class_name("Eligibility::Capital").with_foreign_key(:parent_id).inverse_of(:capital_summary).dependent(:destroy) }
 end
