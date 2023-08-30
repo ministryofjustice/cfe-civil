e5aecf5f945e5a56369bc940ae185c69083d2d1d
Ripan <er.ripankumar@gmail.com>
2023-08-30 23:45:51 +0100

    cleanup not related to ticket

--- app/models/capital_summary.rb
+++ app/models/capital_summary.rb
@@ -5,6 +5,8 @@ class CapitalSummary < ApplicationRecord
            foreign_key: :parent_id,
            inverse_of: :capital_summary,
            dependent: :destroy
+  has_many :disputed_capital_items, -> { disputed }, class_name: "CapitalItem"
+  has_many :disputed_vehicles, -> { disputed }, class_name: "Vehicle"
 
   def summarized_assessment_result
     Utilities::ResultSummarizer.call(eligibilities.map(&:assessment_result))
