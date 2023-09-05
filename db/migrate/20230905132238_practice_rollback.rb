393815a611a1e90623311d2e8acd4d32c593a10f
Stephen Dicks <github@starswan.com>
2023-09-05 14:55:29 +0100

    Revert "practice rollback"
    This reverts commit 0cf84aa7da8911ebfaa3937a819bcb040a9bf39c.

--- db/migrate/20230905132238_practice_rollback.rb
+++
@@ -1,8 +0,0 @@
-class PracticeRollback < ActiveRecord::Migration[7.0]
-  def change
-    create_table :practice_rollback, id: :uuid do |t|
-      t.string :name, null: false
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_132238) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_105118) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -130,12 +130,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_132238) do
     t.string "housing_cost_type"
     t.string "client_id"
     t.index ["disposable_income_summary_id"], name: "index_outgoings_on_disposable_income_summary_id"
-  end
-
-  create_table "practice_rollback", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "proceeding_types", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
