43c6d72d314aea3c9ae13b9445f0b4fb854a3f86
Ripan <er.ripankumar@gmail.com>
2023-09-06 11:31:16 +0100

    Update employment_payment.rb

--- app/models/employment_payment.rb
+++ app/models/employment_payment.rb
@@ -1,6 +1,6 @@
 EmploymentPayment = Data.define(:date, :gross_income, :benefits_in_kind, :tax, :national_insurance, :client_id) do
   def initialize(date:, client_id:, gross_income: 0.0, benefits_in_kind: 0.0, tax: 0.0, national_insurance: 0.0)
-    super(date: date, gross_income: gross_income.to_d, benefits_in_kind: benefits_in_kind.to_d, tax: tax.to_d, national_insurance: national_insurance.to_d, client_id:)
+    super(date:, gross_income: gross_income.to_d, benefits_in_kind: benefits_in_kind.to_d, tax: tax.to_d, national_insurance: national_insurance.to_d, client_id:)
   end
 
   def tax=(tax)
