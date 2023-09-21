b6953a3cd4aa4468609b53a8c3837aacdc501188
Stephen Dicks <github@starswan.com>
2023-09-21 08:39:49 +0100

    tidied up, use .inf in YAML to remove edge case in code and improve readability

--- app/services/calculators/pensioner_capital_disregard_calculator.rb
+++ app/services/calculators/pensioner_capital_disregard_calculator.rb
@@ -3,7 +3,7 @@ module Calculators
     class << self
       def passported_value(submission_date:, date_of_birth:)
         if pensioner?(submission_date:, date_of_birth:)
-          thresholds(submission_date)[:passported]
+          thresholds(submission_date, :passported)
         else
           0
         end
@@ -12,7 +12,9 @@ module Calculators
       def non_passported_value(submission_date:, date_of_birth:, total_disposable_income:)
         if pensioner?(submission_date:, date_of_birth:)
           income = total_disposable_income.to_f
-          thresholds(submission_date)[:monthly_income_values].detect { |value_bands, _banding| income_threshold_applies?(income, value_bands) }.second
+          monthly_income_bands = thresholds(submission_date, :monthly_income_values)
+          value_band_key = monthly_income_bands.keys.detect { |lower, upper| income.between?(lower, upper) }
+          monthly_income_bands.fetch(value_band_key)
         else
           0
         end
@@ -21,7 +23,7 @@ module Calculators
     private
 
       def pensioner?(submission_date:, date_of_birth:)
-        if thresholds(submission_date)[:minimum_age_in_years] == "state_pension_age"
+        if thresholds(submission_date, :minimum_age_in_years) == "state_pension_age"
           submission_date > Calculators::StatePensionDateCalculator.state_pension_date(date_of_birth:)
         else
           earliest_dob_for_pensioner(submission_date) >= date_of_birth
@@ -29,15 +31,11 @@ module Calculators
       end
 
       def earliest_dob_for_pensioner(submission_date)
-        submission_date - thresholds(submission_date)[:minimum_age_in_years].years
+        submission_date - thresholds(submission_date, :minimum_age_in_years).years
       end
 
-      def income_threshold_applies?(income, key_array)
-        (key_array.count.eql?(1) && income >= key_array[0]) || (income >= key_array[0] && income <= key_array[1])
-      end
-
-      def thresholds(submission_date)
-        Threshold.value_for(:pensioner_capital_disregard, at: submission_date)
+      def thresholds(submission_date, key)
+        Threshold.value_for(:pensioner_capital_disregard, at: submission_date).fetch(key)
       end
     end
   end
