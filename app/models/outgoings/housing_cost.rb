fdebce3d51528e562c78d701fe511b6995523cec
Stephen Dicks <github@starswan.com>
2023-09-27 10:10:51 +0100

    housing cost calculator test

--- app/models/outgoings/housing_cost.rb
+++ app/models/outgoings/housing_cost.rb
@@ -5,7 +5,11 @@ module Outgoings
     enum housing_cost_type: enum_hash_for(:rent, :mortgage, :board_and_lodging)
 
     def allowable_amount
-      housing_cost_type == "board_and_lodging" ? (amount / 2).round(2) : amount
+      board_and_lodging? ? (amount / 2).round(2) : amount
+    end
+
+    def board_and_lodging?
+      housing_cost_type == "board_and_lodging"
     end
   end
 end
