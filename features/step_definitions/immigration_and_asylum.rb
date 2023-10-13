2785a50ec7e54fc2b8c09f820793e8afc7a8fa62
Ripan <er.ripankumar@gmail.com>
2023-10-13 13:53:18 +0100

    Update immigration_and_asylum.rb

--- features/step_definitions/immigration_and_asylum.rb
+++ features/step_definitions/immigration_and_asylum.rb
@@ -1,33 +1,47 @@
+class AssessmentData
+  class << self
+    def assessment(level_of_help:)
+      {
+        client_reference_id: "N/A",
+        submission_date: "2022-05-10",
+        level_of_help:,
+      }
+    end
+
+    def applicant
+      {
+        date_of_birth: "1989-12-20",
+        receives_qualifying_benefit: false,
+      }
+    end
+
+    def proceeding_types(ccms_code:)
+      {
+        "proceeding_types": [
+          { ccms_code:, client_involvement_type: "A" },
+        ],
+      }
+    end
+  end
+end
+
 Given("I am undertaking first tier controlled immigration assessment") do
-  @assessment_data = { client_reference_id: "N/A", submission_date: "2022-05-10", level_of_help: "controlled" }
-  @applicant_data = { date_of_birth: "1989-12-20",
-                      involvement_type: "applicant",
-                      has_partner_opponent: false,
-                      receives_qualifying_benefit: false }
-  @proceeding_type_data = { "proceeding_types": [{ ccms_code: "DA001", client_involvement_type: "A" }] }
+  @assessment_data = AssessmentData.assessment(level_of_help: "controlled")
+  @applicant_data = AssessmentData.applicant
+  @proceeding_type_data = AssessmentData.proceeding_types(ccms_code: CFEConstants::IMMIGRATION_PROCEEDING_TYPE_CCMS_CODE)
   @api_version = 6
-  @proceeding_type_data = { "proceeding_types": [{ ccms_code: CFEConstants::IMMIGRATION_PROCEEDING_TYPE_CCMS_CODE, client_involvement_type: "A" }] }
 end
 
 Given("I am undertaking upper tribunal certificated immigration assessment") do
-  @assessment_data = { client_reference_id: "N/A", submission_date: "2022-05-10", level_of_help: "certificated" }
-  @applicant_data = { date_of_birth: "1989-12-20",
-                      involvement_type: "applicant",
-                      has_partner_opponent: false,
-                      receives_qualifying_benefit: false }
-  @proceeding_type_data = { "proceeding_types": [{ ccms_code: "DA001", client_involvement_type: "A" }] }
+  @assessment_data = AssessmentData.assessment(level_of_help: "certificated")
+  @applicant_data = AssessmentData.applicant
+  @proceeding_type_data = AssessmentData.proceeding_types(ccms_code: CFEConstants::IMMIGRATION_PROCEEDING_TYPE_CCMS_CODE)
   @api_version = 6
-  @proceeding_type_data = { "proceeding_types": [{ ccms_code: CFEConstants::IMMIGRATION_PROCEEDING_TYPE_CCMS_CODE, client_involvement_type: "A" }] }
 end
 
 Given("I am undertaking upper tribunal certificated asylum assessment") do
-  @assessment_data = { client_reference_id: "N/A", submission_date: "2022-05-10", level_of_help: "certificated" }
-  @applicant_data = { date_of_birth: "1989-12-20",
-                      involvement_type: "applicant",
-                      has_partner_opponent: false,
-                      receives_qualifying_benefit: false }
-  @proceeding_type_data = { "proceeding_types": [{ ccms_code: "DA001", client_involvement_type: "A" }] }
+  @assessment_data = AssessmentData.assessment(level_of_help: "certificated")
+  @applicant_data = AssessmentData.applicant
+  @proceeding_type_data = AssessmentData.proceeding_types(ccms_code: CFEConstants::ASYLUM_PROCEEDING_TYPE_CCMS_CODE)
   @api_version = 6
-
-  @proceeding_type_data = { "proceeding_types": [{ ccms_code: CFEConstants::ASYLUM_PROCEEDING_TYPE_CCMS_CODE, client_involvement_type: "A" }] }
 end
