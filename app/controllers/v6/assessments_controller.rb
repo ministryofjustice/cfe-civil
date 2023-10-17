3935ca33b5810701aad8b2e05665f518703cd591
Ripan <er.ripankumar@gmail.com>
2023-10-17 18:28:51 +0100

    move remarks creation out of main workflow

--- app/controllers/v6/assessments_controller.rb
+++ app/controllers/v6/assessments_controller.rb
@@ -48,10 +48,12 @@ module V6
           calculation_output = Workflows::MainWorkflow.call(assessment: create.assessment,
                                                             applicant:,
                                                             partner:)
+          add_remarks(assessment: create.assessment, applicant:, partner:, calculation_output:)
         else
           calculation_output = Workflows::MainWorkflow.call(assessment: create.assessment,
                                                             applicant:,
                                                             partner: nil)
+          add_remarks(assessment: create.assessment, applicant:, partner: nil, calculation_output:)
         end
         render json: assessment_decorator_class.new(assessment: create.assessment, calculation_output:, applicant:, partner:, version:).as_json
       else
@@ -60,6 +62,30 @@ module V6
     end
 
   private
+
+    def add_remarks(assessment:, applicant:, partner:, calculation_output:)
+      # we can take the lower threshold from the first eligibility records as they are all the same
+      # lower_capital_threshold = assessment.applicant_capital_summary.eligibilities.first.lower_threshold
+      lower_capital_threshold = calculation_output.capital_subtotals.eligibilities.first.lower_threshold
+
+      new_remarks = RemarkGenerators::Orchestrator.call(employments: applicant.employments,
+                                                        gross_income_summary: assessment.applicant_gross_income_summary,
+                                                        outgoings: applicant.outgoings,
+                                                        liquid_capital_items: applicant.capitals_data.liquid_capital_items,
+                                                        lower_capital_threshold:,
+                                                        child_care_bank: calculation_output.applicant_disposable_income_subtotals.child_care_bank,
+                                                        assessed_capital: calculation_output.capital_subtotals.combined_assessed_capital)
+      if partner.present?
+        new_remarks += RemarkGenerators::Orchestrator.call(employments: partner.employments,
+                                                           gross_income_summary: assessment.partner_gross_income_summary,
+                                                           outgoings: partner.outgoings,
+                                                           liquid_capital_items: partner.capitals_data.liquid_capital_items,
+                                                           lower_capital_threshold:,
+                                                           child_care_bank: calculation_output.partner_disposable_income_subtotals.child_care_bank,
+                                                           assessed_capital: calculation_output.capital_subtotals.combined_assessed_capital)
+      end
+      assessment.add_remarks!(new_remarks)
+    end
 
     def assessment_decorator_class
       Decorators::V6::AssessmentDecorator
