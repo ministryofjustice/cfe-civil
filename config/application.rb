231e0c3c7f58df91fe5b5eb91a5f9bd950375abb
Stephen Dicks <github@starswan.com>
2023-10-26 15:03:26 +0100

    Remove legacy_connection_handling parameter

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module CheckFinancialEligibility
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.active_record.legacy_connection_handling = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
