c9a3e0c09ca0f4e77ac055bda4111aca839c6a60
Ripan <er.ripankumar@gmail.com>
2023-10-31 09:35:14 +0000

    Update payment_checker_spec.rb

--- spec/services/remark_generators/payment_checker_spec.rb
+++ spec/services/remark_generators/payment_checker_spec.rb
@@ -9,20 +9,25 @@ module RemarkGenerators
     let(:gross_income_summary) { create(:gross_income_summary, assessment:) }
     let(:disposable_income_summary) { create(:disposable_income_summary, assessment:) }
     let(:cash_transaction_category) { create(:cash_transaction_category, operation:, name:, gross_income_summary:) }
+    let(:cash_transactions) { gross_income_summary.cash_transactions }
+    let(:regular_transactions) { gross_income_summary.regular_transactions }
 
-    subject(:payment_checker) { described_class.call(cash_transactions: gross_income_summary.cash_transactions, regular_transactions: gross_income_summary.regular_transactions, outgoings:) }
+    subject(:payment_checker) { described_class.call(cash_transactions:, regular_transactions:, outgoings:) }
 
-    context "with payment data" do
-      let(:cash_transactions) { create_list(:cash_transaction, 3, cash_transaction_category:) }
-      let(:regular_transactions) { create_list(:regular_transaction, 3, category:, operation:, gross_income_summary:) }
-      let(:outgoings) { build_list(:priority_debt_repayment_outgoing, 3, client_id: "client_id") }
+    context "with payments" do
+      before do
+        create_list(:cash_transaction, 3, cash_transaction_category:, client_id: "client_id_1")
+        create_list(:regular_transaction, 3, category:, operation:, gross_income_summary:)
+      end
+
+      let(:outgoings) { build_list(:priority_debt_repayment_outgoing, 3, client_id: "client_id_2") }
 
       it "returns remarks array" do
-        expect(payment_checker).to eq [RemarksData.new(type: :priority_debt, issue: :priority_debt, ids: %w[client_id])]
+        expect(payment_checker).to eq [RemarksData.new(type: :priority_debt, issue: :priority_debt, ids: %w[client_id_1 client_id_2])]
       end
     end
 
-    context "without payment data" do
+    context "without payments" do
       let(:outgoings) { [] }
 
       it "returns no remarks" do
