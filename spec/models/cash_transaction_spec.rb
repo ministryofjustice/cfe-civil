3a6277e56ce8138322924d8c787a466ddd0878c2
Stephen Dicks <github@starswan.com>
2023-11-03 10:54:28 +0000

    fix flakey cash transactions spec by using match_array (#405)

--- spec/models/cash_transaction_spec.rb
+++ spec/models/cash_transaction_spec.rb
@@ -10,11 +10,11 @@ describe CashTransaction do
 
   describe "by_operation_and_category" do
     it "display all the cash transactions for benefits 1" do
-      expect(assessment1.applicant_gross_income_summary.cash_transactions.by_operation_and_category(:credit, :benefits)).to eq benefits_transactions1
+      expect(assessment1.applicant_gross_income_summary.cash_transactions.by_operation_and_category(:credit, :benefits)).to match_array benefits_transactions1
     end
 
     it "display all the cash transactions for benefits 2" do
-      expect(assessment2.applicant_gross_income_summary.cash_transactions.by_operation_and_category(:credit, :benefits)).to eq benefits_transactions2
+      expect(assessment2.applicant_gross_income_summary.cash_transactions.by_operation_and_category(:credit, :benefits)).to match_array benefits_transactions2
     end
   end
 end
