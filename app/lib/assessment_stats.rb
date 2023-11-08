dedf9d88ca92792c4cef1281a27efa8460c90109
Ripan <er.ripankumar@gmail.com>
2023-11-08 18:40:37 +0000

    Update assessment_stats.rb

--- app/lib/assessment_stats.rb
+++ app/lib/assessment_stats.rb
@@ -1,105 +1,99 @@
 # RequestLog filtering to generate stats based on https://docs.google.com/spreadsheets/d/1YB_JDQJpq_ZqYRbJ-FH53orLgIcuyBplgrDRl8BgY8M/edit#gid=0
 class AssessmentStats
-  def initialize(user_agent:, http_status:)
-    @user_agent = user_agent
-    @http_status = http_status
+  class Statuses
+    def initialize(response, outcome)
+      @result_summary = response.fetch(:result_summary)
+      @outcome = outcome
+    end
+
+    def gross_income_match?
+      @result_summary.dig(:gross_income, :proceeding_types).any? { |pt| pt[:result] == @outcome }
+    end
+
+    def disposable_income_match?
+      @result_summary.dig(:disposable_income, :proceeding_types).any? { |pt| pt[:result] == @outcome }
+    end
+
+    def capital_match?
+      @result_summary.dig(:capital, :proceeding_types).any? { |pt| pt[:result] == @outcome }
+    end
   end
 
-  def filter_eligibility_results(level_of_help: "controlled", partner: false, passported: false, outcome: "ineligible", reason: %i[])
-    rows = []
-    request_logs.find_each(batch_size: 5000) do |rl|
-      response = rl.response.deep_symbolize_keys
-      request = rl.request.deep_symbolize_keys
-      next unless request[:assessment][:level_of_help] == level_of_help && (request[:applicant][:receives_qualifying_benefit] == passported) && (request[:partner].present? == partner) && rl.http_status == 200
+  class << self
+    def filter_request_log(request_log:, level_of_help:, partner:, passported:)
+      request = request_log.request.deep_symbolize_keys
+      request[:assessment][:level_of_help] == level_of_help && (request[:applicant][:receives_qualifying_benefit] == passported) && (request[:partner].present? == partner)
+    end
 
-      # filter response
-      case reason.sort
-      when %i[gross_income].sort
-        if match?(response, :gross_income, outcome)
-          rows << rl.id
-        end
-      when %i[disposable_income].sort
-        if match?(response, :disposable_income, outcome)
-          rows << rl.id
-        end
-      when %i[capital].sort
-        if match?(response, :capital, outcome)
-          rows << rl.id
-        end
-      when %i[gross_income disposable_income].sort
-        if match?(response, :gross_income, outcome) && match?(response, :disposable_income, outcome)
-          rows << rl.id
-        end
-      when %i[disposable_income capital].sort
-        if match?(response, :disposable_income, outcome) && match?(response, :capital, outcome)
-          rows << rl.id
-        end
-      when %i[gross_income capital].sort
-        if match?(response, :gross_income, outcome) && match?(response, :capital, outcome)
-          rows << rl.id
-        end
-      when %i[gross_income disposable_income capital].sort
-        if match?(response, :gross_income, outcome) && match?(response, :disposable_income, outcome) && match?(response, :capital, outcome)
-          rows << rl.id
-        end
-      else
-        raise "Invalid reason: #{reason} "
+    # filter out the request logs so that they meet the specifiied criteria
+    def filter_request_logs(request_logs:, level_of_help:, partner: false, passported: false)
+      request_logs.select { |rl| filter_request_log(request_log: rl, partner:, passported:, level_of_help:) }.map { |rl| rl.response.deep_symbolize_keys }
+    end
+
+    def count_gross_results(responses:, outcome:)
+      responses.count do |response|
+        status = Statuses.new response, outcome
+        status.gross_income_match?
       end
     end
 
-    {
-      count: rows.size,
-      total: request_logs.size,
-      percentage: (rows.size * 100).to_f / request_logs.size,
-    }
-  end
+    def count_disposable_results(responses:, outcome:)
+      responses.count do |response|
+        status = Statuses.new response, outcome
+        status.disposable_income_match?
+      end
+    end
 
-private
+    def count_capital_results(responses:, outcome:)
+      responses.count do |response|
+        status = Statuses.new response, outcome
+        status.capital_match?
+      end
+    end
 
-  def match?(response, reason, outcome)
-    response[:result_summary][reason][:proceeding_types].any? { |pt| pt[:result] == outcome }
-  end
+    def count_gross_disposable_results(responses:, outcome:)
+      responses.count do |response|
+        status = Statuses.new response, outcome
+        status.gross_income_match? && status.disposable_income_match?
+      end
+    end
 
-  def request_logs
-    RequestLog.where(http_status: @http_status).where("user_agent LIKE ?", @user_agent)
+    def count_gross_capital_results(responses:, outcome:)
+      responses.count do |response|
+        status = Statuses.new response, outcome
+        status.gross_income_match? && status.capital_match?
+      end
+    end
+
+    def count_disposable_capital_results(responses:, outcome:)
+      responses.count do |response|
+        status = Statuses.new response, outcome
+        status.disposable_income_match? && status.capital_match?
+      end
+    end
+
+    def count_gross_disposable_capital_results(responses:, outcome:)
+      responses.count do |response|
+        status = Statuses.new response, outcome
+        status.gross_income_match? && status.disposable_income_match? && status.capital_match?
+      end
+    end
+
+    def output_counts(row_count:, total_count:)
+      {
+        count: row_count,
+        total: total_count,
+        percentage: ((row_count * 100).to_f / total_count).round(2),
+      }
+    end
+
+    def all_request_logs(user_agent)
+      RequestLog.where(http_status: 200).where("user_agent LIKE ?", user_agent) # .find_each(batch_size: 5000)
+    end
   end
 end
 
-# assessment_stats = AssessmentStats.new(user_agent: "ccq%(production)", http_status: 200)
-
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: false, passported: false, outcome: "ineligible", reason: %i[gross_income])
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: false, passported: false, outcome: "ineligible", reason: %i[disposable_income])
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: false, passported: false, outcome: "ineligible", reason: %i[capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: false, passported: false, outcome: "ineligible", reason: %i[gross_income disposable_income])
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: false, passported: false, outcome: "ineligible", reason: %i[gross_income capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: false, passported: false, outcome: "ineligible", reason: %i[disposable_income capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: false, passported: false, outcome: "ineligible", reason: %i[gross_income disposable_income capital])
-
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: false, passported: false, outcome: "ineligible", reason: %i[gross_income])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: false, passported: false, outcome: "ineligible", reason: %i[disposable_income])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: false, passported: false, outcome: "ineligible", reason: %i[capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: false, passported: false, outcome: "ineligible", reason: %i[gross_income disposable_income])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: false, passported: false, outcome: "ineligible", reason: %i[gross_income capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: false, passported: false, outcome: "ineligible", reason: %i[disposable_income capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: false, passported: false, outcome: "ineligible", reason: %i[gross_income disposable_income capital])
-
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: true, passported: false, outcome: "ineligible", reason: %i[gross_income])
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: true, passported: false, outcome: "ineligible", reason: %i[disposable_income])
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: true, passported: false, outcome: "ineligible", reason: %i[capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: true, passported: false, outcome: "ineligible", reason: %i[gross_income disposable_income])
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: true, passported: false, outcome: "ineligible", reason: %i[gross_income capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: true, passported: false, outcome: "ineligible", reason: %i[disposable_income capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: true, passported: false, outcome: "ineligible", reason: %i[gross_income disposable_income capital])
-
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: true, passported: false, outcome: "ineligible", reason: %i[gross_income])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: true, passported: false, outcome: "ineligible", reason: %i[disposable_income])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: true, passported: false, outcome: "ineligible", reason: %i[capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: true, passported: false, outcome: "ineligible", reason: %i[gross_income disposable_income])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: true, passported: false, outcome: "ineligible", reason: %i[gross_income capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: true, passported: false, outcome: "ineligible", reason: %i[disposable_income capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: true, passported: false, outcome: "ineligible", reason: %i[gross_income disposable_income capital])
-
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: false, passported: true, outcome: "ineligible", reason: %i[capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "controlled", partner: true, passported: true, outcome: "ineligible", reason: %i[capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: false, passported: true, outcome: "ineligible", reason: %i[capital])
-# assessment_stats.filter_eligibility_results(level_of_help: "certificated", partner: true, passported: true, outcome: "ineligible", reason: %i[capital])
+# request_logs = AssessmentStats.all_request_logs("ccq%(production)")
+# responses = AssessmentStats.filter_request_logs(request_logs: request_logs, level_of_help: "controlled", partner: false, passported: false)
+# count = AssessmentStats.count_gross_results(responses: responses, outcome: "ineligible")
+# AssessmentStats.output_counts(row_count: count, total_count: request_logs.count)
