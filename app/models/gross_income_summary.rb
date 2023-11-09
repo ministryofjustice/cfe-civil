49866ba1508636d4add30589c71bdf5ae199fe76
Ripan <ripan.kumar@digital.justice.gov.uk>
2023-11-09 11:12:38 +0000

    fix cash transactions ordering (#407)

--- app/models/gross_income_summary.rb
+++ app/models/gross_income_summary.rb
@@ -4,8 +4,7 @@ class GrossIncomeSummary < ApplicationRecord
   has_many :regular_transactions, dependent: :destroy
   has_many :irregular_income_payments, dependent: :destroy
   has_many :cash_transaction_categories, dependent: :destroy
-  has_many :cash_transactions, through: :cash_transaction_categories, dependent: :destroy
-
+  has_many :cash_transactions, -> { order(date: :asc) }, through: :cash_transaction_categories, dependent: :destroy
   has_many :student_loan_payments, -> { student_loan }, class_name: "IrregularIncomePayment"
   has_many :unspecified_source_payments, -> { unspecified_source }, class_name: "IrregularIncomePayment"
 end
--- spec/models/cash_transaction_spec.rb
+++ spec/models/cash_transaction_spec.rb
@@ -1,20 +1,16 @@
 require "rails_helper"
 
 describe CashTransaction do
-  let(:assessment1) { create :assessment, :with_gross_income_summary_and_records }
-  let(:assessment2) { create :assessment, :with_gross_income_summary_and_records }
-  let(:benefits_category1) { assessment1.applicant_gross_income_summary.cash_transaction_categories.detect { |cat| cat.name == "benefits" } }
-  let(:benefits_category2) { assessment2.applicant_gross_income_summary.cash_transaction_categories.detect { |cat| cat.name == "benefits" } }
-  let!(:benefits_transactions1) { benefits_category1.cash_transactions.order(:date) }
-  let!(:benefits_transactions2) { benefits_category2.cash_transactions.order(:date) }
+  let(:assessment) { create :assessment }
+  let(:gross_income_summary) { create :gross_income_summary, assessment: }
+  let(:cash_transaction_category) { create :cash_transaction_category, operation: "debit", name: "child_care", gross_income_summary: }
+  let(:cash_transaction_1) { create :cash_transaction, cash_transaction_category:, date: Date.new(2023, 4, 0o1) }
+  let(:cash_transaction_2) { create :cash_transaction, cash_transaction_category:, date: Date.new(2023, 3, 0o1) }
+  let(:cash_transaction_3) { create :cash_transaction, cash_transaction_category:, date: Date.new(2023, 2, 0o1) }
 
-  describe "by_operation_and_category" do
-    it "display all the cash transactions for benefits 1" do
-      expect(assessment1.applicant_gross_income_summary.cash_transactions.by_operation_and_category(:credit, :benefits)).to match_array benefits_transactions1
-    end
-
-    it "display all the cash transactions for benefits 2" do
-      expect(assessment2.applicant_gross_income_summary.cash_transactions.by_operation_and_category(:credit, :benefits)).to match_array benefits_transactions2
+  describe "#by_operation_and_category" do
+    it "display all the cash transactions in ascending order" do
+      expect(gross_income_summary.cash_transactions.by_operation_and_category(:debit, :child_care)).to eq [cash_transaction_3, cash_transaction_2, cash_transaction_1]
     end
   end
 end
