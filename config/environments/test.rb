e4bbd811dcbbf11ee4cec158ac5036a949238deb
Stephen Dicks <github@starswan.com>
2023-11-17 14:49:53 +0000

    Test exception settings were the mirror image of correct ones (#438)

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  config.action_dispatch.show_exceptions = true
+  config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
