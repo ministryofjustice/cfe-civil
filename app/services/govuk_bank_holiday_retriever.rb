3d71c65b9079178abbe7b203ebefc4f04b5fa928
Stephen Dicks <github@starswan.com>
2024-01-05 15:23:31 +0000

    Use Rails.cache for caching bank holiday response (#491)

--- app/services/govuk_bank_holiday_retriever.rb
+++ app/services/govuk_bank_holiday_retriever.rb
@@ -1,30 +1,29 @@
 class GovukBankHolidayRetriever
-  def self.dates
-    new.dates(CFEConstants::GOVUK_BANK_HOLIDAY_DEFAULT_GROUP)
-  end
+  # Date and bank holidays
+  #
+  GOVUK_BANK_HOLIDAY_API_URL = "https://www.gov.uk/bank-holidays.json".freeze
+  GOVUK_BANK_HOLIDAY_DEFAULT_GROUP = "england-and-wales".freeze
 
-  def dates(group)
-    JSON.parse(response.body).dig(group, "events")&.pluck("date")
-  end
-
-private
-
-  def response
-    # The Faraday::HttpCache middleware is slightly 'broken' as it doesn't respect the TTL of the
-    # response, and relies on the expiry time set on the 'store' object passed to
-    # it. k8s pods get recycled regularly anyway, so the expiry time is unlikely
-    # to be exceeded
-    store = ActiveSupport::Cache.lookup_store(:file_store, "tmp/cache", expires_in: 10.days)
-    client = Faraday.new do |builder|
-      builder.use Faraday::HttpCache, store:, strategy: Faraday::HttpCache::Strategies::ByUrl, logger: Rails.logger
-      builder.adapter Faraday.default_adapter
-      builder.response :raise_error
+  class << self
+    def dates
+      JSON.parse(response_body).dig(GOVUK_BANK_HOLIDAY_DEFAULT_GROUP, "events").pluck("date")
     end
 
-    @response ||= client.get(uri)
-  end
+  private
 
-  def uri
-    URI.parse(CFEConstants::GOVUK_BANK_HOLIDAY_API_URL)
+    def response_body
+      client = Faraday.new do |builder|
+        builder.adapter Faraday.default_adapter
+        builder.response :raise_error
+      end
+
+      Rails.cache.fetch(uri, expires_in: 10.days) do
+        client.get(uri).body
+      end
+    end
+
+    def uri
+      URI.parse(GOVUK_BANK_HOLIDAY_API_URL)
+    end
   end
 end
