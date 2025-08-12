795a427d4a88b23ca9c41cd3d3f758975606b6bb
ColinBruce <colin.bruce@digital.justice.gov.uk>
2025-08-12 08:56:55 +0100

    AP-6164: run rails app:update
    Then reviewed all changes and restored anything that was
    overwritten but remains relevant

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,16 +5,6 @@
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
 Rails.application.configure do
-  #   config.content_security_policy do |policy|
-  #     policy.default_src :self, :https
-  #     policy.font_src    :self, :https, :data
-  #     policy.img_src     :self, :https, :data
-  #     policy.object_src  :none
-  #     policy.script_src  :self, :https
-  #     policy.style_src   :self, :https
-  #     # Specify URI for violation reports
-  #     # policy.report_uri "/csp-violation-report-endpoint"
-  #   end
   config.content_security_policy do |policy|
     policy.default_src :self
     policy.font_src :self, :data
@@ -24,10 +14,10 @@ Rails.application.configure do
     policy.style_src :self, :unsafe_inline
   end
   #
-  #   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
-  #   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  #   config.content_security_policy_nonce_directives = %w(script-src style-src)
+  # # Generate session nonces for permitted importmap, inline scripts, and inline styles.
+  # config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  # config.content_security_policy_nonce_directives = %w(script-src style-src)
   #
-  #   # Report violations without enforcing the policy.
-  #   # config.content_security_policy_report_only = true
+  # # Report violations without enforcing the policy.
+  # # config.content_security_policy_report_only = true
 end
