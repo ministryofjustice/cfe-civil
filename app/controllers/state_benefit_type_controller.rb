8ef16ec3b830689a23694225e2bfe0afb40c59ea
Katharine Ahern <katharine.ahern@justice.gov.uk>
2025-09-03 13:09:02 +0100

    temp commit to test alerting

--- app/controllers/state_benefit_type_controller.rb
+++ app/controllers/state_benefit_type_controller.rb
@@ -1,5 +1,7 @@
 class StateBenefitTypeController < ApplicationController
   def index
+    raise "Katharine testing" if Rails.env.production?
+
     render json: StateBenefitType.as_cfe_json
   end
 end
